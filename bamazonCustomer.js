081c987fde273af685c15197ba0d9cdba303142c
jmartinez <jcmartinez1925@gmail.com>
2019-09-07 13:13:31 -0700

    rework app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,54 +15,62 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
-//handle error or show products
+//handle error
 connection.connect(function (err) {
     if (err) throw err;
 
-    displayItems();
+    return;
 });
 
 //function to show products
-var displayItems = function () {
-
-    connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-
-        for (var i = 0; i < res.length; i++) {
-
-            console.log("Item # " + res[i].item_id + " || Product: " + res[i].product_name + " || Price: " + res[i].price);
-
-        }
-
-        promptCustomer();
-    });
-};
-
-//function to ask customer for input
-var promptCustomer = function () {
-    inquirer.prompt([{
-        name: "id",
+connection.query('SELECT * FROM `products`', function (err, results, fields) {
+    if (err) {
+      console.log(err);
+    }
+    for (var i=0; i<results.length; i++) {
+      console.log(results[i].item_id + " " + results[i].product_name + " [" + results[i].price + "]");
+    }
+    // Prompt user to select a product and enter desired quantity
+    function buyPrompt() {
+      inquirer.prompt( [{
+        name: "itemId",
         type: "input",
-        message: "Please enter id # you would like to purchase.",
-        validate: function (value) {
-            if (isNaN(value) === false) {
-                return true;
-            }
-            return false;
-        }
-    },
-    {
-        name: "units",
+        message: "Enter the id of the product you'd like to buy."
+      }, {
+        name: "quantity",
         type: "input",
-        message: "How many units would you like to purchase?",
-        validate: function (value) {
-            if (isNaN(value) === false) {
-                return true;
+        message: "How many would you like to purchase?"
+      }]).then(function(answer) {
+        for (var i=0; i<results.length; i++) {
+          if (results[i].item_id === parseInt(answer.itemId)) {
+            // If order quantity is too high, notify user of insufficient stock
+            if (results[i].stock_quantity < parseInt(answer.quantity)) {
+              console.log("Insufficient stock!");
+              buyPrompt();
+            } else {
+              // Calculate order total and remaining stock
+              var total = parseFloat(answer.quantity*results[i].price).toFixed(2);
+              var newStock = results[i].stock_quantity - answer.quantity;
+  
+              // Construct query to update stock
+              var updateStock = 'UPDATE `products` SET `stock_quantity` = ' + newStock + ' WHERE `item_id` = ' + answer.itemId
+              connection.query(updateStock, function(err, result) {
+                if (err) {
+                  console.log(err);
+                } else {
+                  console.log(result.affectedRows + " product updated");
+                }
+              });
+  
+              // Notify user of successful purchase
+              console.log("You have purchased " + answer.quantity + " " + results[i].product_name);
+              console.log("Your order total is " + total);
             }
-            return false
+          }
         }
+      });
+    }
+    buyPrompt();
+  });
+ 
 
-    }]).then(function(response) {
-
-    })
-}
