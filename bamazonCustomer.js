a8a5415ca7590e65620e1588f59790994f6bf8eb
jmartinez <jcmartinez1925@gmail.com>
2019-09-07 13:29:55 -0700

    change display of items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,37 +23,42 @@ connection.connect(function (err) {
 });
 
 //function to show products
-connection.query('SELECT * FROM `products`', function (err, results, fields) {
+connection.query('SELECT * FROM `products`', function (err, res) {
     if (err) {
       console.log(err);
     }
-    for (var i=0; i<results.length; i++) {
-      console.log(results[i].item_id + " " + results[i].product_name + " [" + results[i].price + "]");
+    for (var i = 0; i < res.length; i++) {
+      console.log("Item: " + res[i].item_id + " | Product: " + res[i].product_name + 
+      " | Price: " + res[i].price + " | Quantity Available: " + res[i].stock_quantity);
     }
-    // Prompt user to select a product and enter desired quantity
-    function buyPrompt() {
+
+    //prompt customer for input
+    function promptCustomer() {
       inquirer.prompt( [{
-        name: "itemId",
+        name: "id",
         type: "input",
-        message: "Enter the id of the product you'd like to buy."
-      }, {
+        message: "Enter the number to the item you want to buy."
+      }, 
+      {
         name: "quantity",
         type: "input",
         message: "How many would you like to purchase?"
-      }]).then(function(answer) {
-        for (var i=0; i<results.length; i++) {
-          if (results[i].item_id === parseInt(answer.itemId)) {
+
+      }])
+      .then(function(answer) {
+        for (var i=0; i<res.length; i++) {
+          if (res[i].item_id === parseInt(answer.id)) {
             // If order quantity is too high, notify user of insufficient stock
-            if (results[i].stock_quantity < parseInt(answer.quantity)) {
+            if (res[i].stock_quantity < parseInt(answer.quantity)) {
               console.log("Insufficient stock!");
-              buyPrompt();
+              promptCustomer();
             } else {
               // Calculate order total and remaining stock
-              var total = parseFloat(answer.quantity*results[i].price).toFixed(2);
-              var newStock = results[i].stock_quantity - answer.quantity;
+              var total = parseFloat(answer.quantity*res[i].price).toFixed(2);
+              var newStock = res[i].stock_quantity - answer.quantity;
   
               // Construct query to update stock
-              var updateStock = 'UPDATE `products` SET `stock_quantity` = ' + newStock + ' WHERE `item_id` = ' + answer.itemId
+              var updateStock = 'UPDATE `products` SET `stock_quantity` = ' + newStock + ' WHERE `item_id` = ' + answer.id
               connection.query(updateStock, function(err, result) {
                 if (err) {
                   console.log(err);
@@ -63,14 +68,14 @@ connection.query('SELECT * FROM `products`', function (err, results, fields) {
               });
   
               // Notify user of successful purchase
-              console.log("You have purchased " + answer.quantity + " " + results[i].product_name);
+              console.log("You have purchased " + answer.quantity + " " + res[i].product_name);
               console.log("Your order total is " + total);
             }
           }
         }
       });
     }
-    buyPrompt();
+    promptCustomer();
   });
  
 
